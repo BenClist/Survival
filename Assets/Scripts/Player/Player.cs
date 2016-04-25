de6abdb94473435cbc9f88a90173b8628148b7c2
Ben Clist <ben@benclist.com>
2016-04-25 20:45:37 +1200

    Shooting
    Added basic shooting system.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player : MonoBehaviour {
 
     [SerializeField]
     GameObject deathcanvas;
+
     [SerializeField]
     float health = 100f;
+	[SerializeField]
+	float hunger = 100f;
+	[SerializeField]
+	float thirst = 100f;
+
+	[SerializeField]
+	Image healthBar;
+	[SerializeField]
+	Image hungerBar;
+	[SerializeField]
+	Image thirstBar;
+
+	[SerializeField]
+	float healthSpeed = 0.25f;
+	[SerializeField]
+	float hungerSpeed = 0.1f;
+	[SerializeField]
+	float thirstSpeed = 0.25f;
 
     private Vector3 spawnPoint;
+	private bool isDying;
 
     private void Start()
     {
@@ -19,6 +40,31 @@ public class Player : MonoBehaviour {
     private void Update()
     {
         CheckDeath();
+
+		if (hunger > 0) 
+		{
+			hunger -= Time.deltaTime * hungerSpeed;           
+		}
+		if (thirst > 0) 
+		{
+			thirst -= Time.deltaTime * thirstSpeed;           
+		}
+		if (hunger <= 0 || thirst <= 0) {
+			isDying = true;
+		} else 
+		{
+			isDying = false;
+		}
+		if (isDying) 
+		{
+			health -= Time.deltaTime * healthSpeed;
+		}
+			
+
+
+		healthBar.fillAmount = health / 100;
+		hungerBar.fillAmount = hunger / 100;
+		thirstBar.fillAmount = thirst / 100;
     }
 
     private void CheckDeath()
@@ -38,6 +84,8 @@ public class Player : MonoBehaviour {
     {
         deathcanvas.SetActive(false);
         transform.position = spawnPoint;
-        health = 100;
+        health = 100f;
+		hunger = 100f;
+		thirst = 100f;
     }
 }
---
+++ Assets/Scripts/Weapons/ShootingSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingSystem : MonoBehaviour {
+
+	private AdvancedEnemyAI enemy;
+
+	[SerializeField]
+	float weaponRange = 100f;
+	[SerializeField]
+	int minDamage = 15;
+	[SerializeField]
+	int maxDamage = 30;
+	[SerializeField]
+	Camera FPSCamera;
+
+	private void Update () {
+		Ray ray = FPSCamera.ScreenPointToRay(new Vector2(Screen.width/2, Screen.height/2));
+		RaycastHit hitInfo;
+
+		Debug.DrawRay (ray.origin, ray.direction * weaponRange, Color.green);
+
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+
+			if(Physics.Raycast(ray, out hitInfo, weaponRange)){
+				if(hitInfo.collider.tag=="Enemy"){
+					enemy = hitInfo.collider.GetComponent<AdvancedEnemyAI> ();
+					enemy.TakeDamage (Damage());
+					Debug.Log ("We attacked an enemy!");
+				}
+			}
+		}
+	}
+
+	private int Damage(){
+		return Random.Range (minDamage, maxDamage);
+	}
+}
+			
